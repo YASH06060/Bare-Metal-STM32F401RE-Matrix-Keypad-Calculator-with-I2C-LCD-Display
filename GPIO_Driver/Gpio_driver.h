5c48ce6ff27b85630315e0f67780a0838190da34
YASH06060 <ygour739@gmail.com>
2025-08-18 12:56:57 +0530

    Create Gpio_driver.h

---
+++ GPIO_Driver/Gpio_driver.h
@@ -0,0 +1,52 @@
+#include "stm32f401re.h"
+
+typedef struct {
+    uint8_t pinNumber;
+    uint8_t mode;        // GPIO_MODE_IN=0, GPIO_MODE_OUT=1, etc
+    uint8_t speed;       // Output speed config
+    uint8_t outputType;  // Push-pull, open drain
+    uint8_t pull;        // None, pull-up, pull-down
+    uint8_t altFuncSel;  // Alternate function number (if mode == AF)
+} GPIO_Config_Mod_t;
+
+typedef struct {
+    GPIO_Regs_t *port;
+    GPIO_Config_Mod_t config;
+} GPIO_Handle_Mod_t;
+
+#define GPIO_MODE_IN    0
+#define GPIO_MODE_OUT   1
+#define GPIO_MODE_ALTFN 2
+#define GPIO_MODE_ANALOG 3
+
+#define GPIO_OT_PP      0
+#define GPIO_OT_OD      1
+
+#define GPIO_SPEED_LOW  0
+#define GPIO_SPEED_MED  1
+#define GPIO_SPEED_HIGH 2
+#define GPIO_SPEED_VHIGH 3
+
+#define GPIO_NO_PULL    0
+#define GPIO_PULL_UP    1
+#define GPIO_PULL_DOWN  2
+
+#define GPIO_PIN_SET    1
+#define GPIO_PIN_RESET  0
+
+// API prototypes
+
+void GPIO_EnableClock(GPIO_Regs_t *GPIOx, uint8_t en_dis);
+
+void GPIO_Init(GPIO_Handle_Mod_t *handle);
+void GPIO_DeInit(GPIO_Regs_t *GPIOx);
+
+uint8_t GPIO_ReadPin(GPIO_Regs_t *GPIOx, uint8_t pin);
+uint16_t GPIO_ReadPort(GPIO_Regs_t *GPIOx);
+
+void GPIO_WritePin(GPIO_Regs_t *GPIOx, uint8_t pin, uint8_t val);
+void GPIO_WritePort(GPIO_Regs_t *GPIOx, uint16_t val);
+
+void GPIO_TogglePin(GPIO_Regs_t *GPIOx, uint8_t pin);
+
+
