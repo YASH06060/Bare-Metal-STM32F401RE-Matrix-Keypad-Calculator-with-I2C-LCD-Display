f5e86e57a6b54583815254615e5d8909064512c2
YASH06060 <ygour739@gmail.com>
2025-08-19 11:47:42 +0530

    Delete LCD_i2C_driver directory

--- LCD_i2C_driver/LCD_i2c_driver.h
+++
@@ -1,8 +0,0 @@
-#include "i2c_driver.h"
-
-#define LCD_ADDR        0x27  
-
-void LCD_I2C_Init(I2C_Regs_t *I2Cx, uint8_t address);
-void LCD_I2C_SendCmd(I2C_Regs_t *I2Cx, uint8_t address, uint8_t cmd);
-void LCD_I2C_SendData(I2C_Regs_t *I2Cx, uint8_t address, uint8_t data);
-void LCD_I2C_Print(I2C_Regs_t *I2Cx, uint8_t address, const char *str);
