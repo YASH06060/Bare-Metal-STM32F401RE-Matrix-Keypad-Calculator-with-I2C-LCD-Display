af6e2b2b1fe79cd399381e691ff548c95528fac7
YASH06060 <ygour739@gmail.com>
2025-08-19 11:48:39 +0530

    Update stm32f401re.h

--- stm32f401re.h
+++ stm32f401re.h
@@ -1,32 +1,30 @@
-/*
- * stm32f401re_mod.h
- * Modified low-level definition header for STM32F401RE
- */
 
-#ifndef INC_STM32F401RE_H_
-#define INC_STM32F401RE_H_
 
 #include <stdint.h>
 
-#define FLASH_BASE 0x08000000U
-#define SRAM1_BASE 0x20000000U
-#define ROM_BASE   0x1FFF0000U
+#define FLASH_BASE      0x08000000U
+#define SRAM1_BASE      0x20000000U
+#define ROM_BASE        0x1FFF0000U
+#define PERIPH_BASE     0x40000000U
+#define AHB1_BASE       0x40020000U
+#define APB1_BASE       PERIPH_BASE
+#define APB2_BASE       0x40010000U
 
-#define PERIPH_BASE 0x40000000U
-#define AHB1_BASE   0x40020000U
-#define APB1_BASE   PERIPH_BASE
-#define APB2_BASE   0x40010000U
+// GPIO base addresses
+#define GPIOA_BASE      (AHB1_BASE + 0x0000U)
+#define GPIOB_BASE      (AHB1_BASE + 0x0400U)
+#define GPIOC_BASE      (AHB1_BASE + 0x0800U)
+#define GPIOD_BASE      (AHB1_BASE + 0x0C00U)
+#define GPIOE_BASE      (AHB1_BASE + 0x1000U)
+#define GPIOH_BASE      (AHB1_BASE + 0x1C00U)
 
-// GPIO Base Addresses
-#define GPIOA_BASE (AHB1_BASE + 0x0000U)
-#define GPIOB_BASE (AHB1_BASE + 0x0400U)
-#define GPIOC_BASE (AHB1_BASE + 0x0800U)
-#define GPIOD_BASE (AHB1_BASE + 0x0C00U)
-#define GPIOE_BASE (AHB1_BASE + 0x1000U)
-#define GPIOH_BASE (AHB1_BASE + 0x1C00U)
+// RCC base address
+#define RCC_BASE        (AHB1_BASE + 0x3800U)
 
-// RCC Base Address
-#define RCC_BASE   (AHB1_BASE + 0x3800U)
+// I2C base addresses
+#define I2C1_BASE       (APB1_BASE + 0x5400U)
+#define I2C2_BASE       (APB1_BASE + 0x5800U)
+#define I2C3_BASE       (APB1_BASE + 0x5C00U)
 
 // GPIO register structure
 typedef struct {
@@ -53,39 +51,55 @@ typedef struct {
     uint32_t RESERVED0[2];
     volatile uint32_t APB1RSTR;
     volatile uint32_t APB2RSTR;
-    uint32_t RESERVED1[2];
+    uint32_t RESERVED1[1];
     volatile uint32_t AHB1ENR;
     volatile uint32_t AHB2ENR;
-    uint32_t RESERVED2[2];
+    uint32_t RESERVED2[1];
     volatile uint32_t APB1ENR;
     volatile uint32_t APB2ENR;
-    uint32_t RESERVED3[2];
+    uint32_t RESERVED3[1];
     volatile uint32_t AHB1LPENR;
     volatile uint32_t AHB2LPENR;
-    uint32_t RESERVED4[2];
+    uint32_t RESERVED4[1];
     volatile uint32_t APB1LPENR;
     volatile uint32_t APB2LPENR;
-    uint32_t RESERVED5[2];
+    uint32_t RESERVED5[1];
     volatile uint32_t BDCR;
     volatile uint32_t CSR;
-    uint32_t RESERVED6[2];
+    uint32_t RESERVED6[1];
     volatile uint32_t SSCGR;
     volatile uint32_t PLLI2SCFGR;
     uint32_t RESERVED7;
     volatile uint32_t DCKCFGR;
 } RCC_Regs_t;
 
+// I2C register structure
+typedef struct {
+    volatile uint32_t CR1;
+    volatile uint32_t CR2;
+    volatile uint32_t OAR1;
+    volatile uint32_t OAR2;
+    volatile uint32_t DR;
+    volatile uint32_t SR1;
+    volatile uint32_t SR2;
+    volatile uint32_t CCR;
+    volatile uint32_t TRISE;
+    volatile uint32_t FLTR;
+} I2C_Regs_t;
+
 // Peripheral pointers
-#define GPIOA ((GPIO_Regs_t *) GPIOA_BASE)
-#define GPIOB ((GPIO_Regs_t *) GPIOB_BASE)
-#define GPIOC ((GPIO_Regs_t *) GPIOC_BASE)
-#define GPIOD ((GPIO_Regs_t *) GPIOD_BASE)
-#define GPIOE ((GPIO_Regs_t *) GPIOE_BASE)
-#define GPIOH ((GPIO_Regs_t *) GPIOH_BASE)
+#define GPIOA   ((GPIO_Regs_t *) GPIOA_BASE)
+#define GPIOB   ((GPIO_Regs_t *) GPIOB_BASE)
+#define GPIOC   ((GPIO_Regs_t *) GPIOC_BASE)
+#define GPIOD   ((GPIO_Regs_t *) GPIOD_BASE)
+#define GPIOE   ((GPIO_Regs_t *) GPIOE_BASE)
+#define GPIOH   ((GPIO_Regs_t *) GPIOH_BASE)
+#define RCC     ((RCC_Regs_t *) RCC_BASE)
+#define I2C1    ((I2C_Regs_t *) I2C1_BASE)
+#define I2C2    ((I2C_Regs_t *) I2C2_BASE)
+#define I2C3    ((I2C_Regs_t *) I2C3_BASE)
 
-#define RCC   ((RCC_Regs_t *) RCC_BASE)
-
-#define ENABLED  1
+#define ENABLED 1
 #define DISABLED 0
 
-#endif /* INC_STM32F401RE_MOD_H_ */
+
